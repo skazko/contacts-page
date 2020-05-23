9763933251a945f14b193e9bf530e9f6cbb0db4e
Slava <kreeon@ya.ru>
2020-05-23 22:19:47 +0300

    update Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -49,41 +49,104 @@ const _Modal: React.FC<ModalProps> = ({ addContact, deleteContact }) => {
   return (
     <div onClick={goBack} className="Modal">
       <div onClick={stop} className="Modal-Body">
-        <div className="input">
-          <label htmlFor="">
-            Имя:
+        <div className="Modal-Content">
+          <div className="Modal-Header">
+            <button
+              onClick={goBack}
+              className="waves-effect"
+              style={{
+                float: 'right',
+                backgroundColor: 'transparent',
+                border: 'none',
+                width: '30px',
+                height: '30px',
+                lineHeight: '30px',
+                borderRadius: '50%',
+                padding: 0,
+              }}
+            >
+              <i
+                className="material-icons"
+                style={{
+                  lineHeight: '30px',
+                  verticalAlign: 'middle',
+                }}
+              >
+                close
+              </i>
+            </button>
+            <h4>
+              {contact.id ? 'Редактирование контакта' : 'Создание контакта'}
+            </h4>
+          </div>
+          <div className="input-field">
+            <i className="material-icons prefix">person</i>
             <input
               onChange={update}
               type="text"
               name="name"
               value={contact.name}
+              id="contact-name"
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="">
-            Телефон:
+            <label
+              className={contact.id ? 'active' : ''}
+              htmlFor="contact-name"
+            >
+              Имя
+            </label>
+          </div>
+          <div className="input-field">
+            <i className="material-icons prefix">contact_phone</i>
             <input
               onChange={update}
               type="text"
               name="phone"
               value={contact.phone}
+              id="contact-phone"
             />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="">
-            Почта:
+            <label
+              className={contact.id ? 'active' : ''}
+              htmlFor="contact-phone"
+            >
+              Телефон
+            </label>
+          </div>
+          <div className="input-field">
+            <i className="material-icons prefix">contact_mail</i>
             <input
               onChange={update}
               type="text"
               name="email"
               value={contact.email}
+              id="contact-email"
             />
-          </label>
+            <label
+              className={contact.id ? 'active' : ''}
+              htmlFor="contact-email"
+            >
+              Почта
+            </label>
+          </div>
+          <div className="Modal-Footer text-right">
+            <button
+              disabled={!contact.id}
+              onClick={onDelete}
+              className="btn-flat red-text text-darken-4"
+            >
+              Удалить
+              <i className="material-icons right">delete_forever</i>
+            </button>
+            <span> </span>
+            <button
+              disabled={!contact.name || !contact.phone || !contact.email}
+              className="btn waves-effect waves-light blue darken-4"
+              onClick={save}
+            >
+              Сохранить
+              <i className="material-icons right">save</i>
+            </button>
+          </div>
         </div>
-        <button onClick={save}>Сохранить</button>
-        <button onClick={onDelete}>Удалить</button>
       </div>
     </div>
   );
